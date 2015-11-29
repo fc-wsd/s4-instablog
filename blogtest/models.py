c181f77acc55f679fe7db3a43ee10adf9bec3f66
Kay Cha <kay@hannal.net>
2015-11-29 17:13:53 +0900

    fix 테스트용 모델 기본 뼈대

--- blogtest/models.py
+++ blogtest/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 
 class Post(models.Model):
     """게시물 정보를 담는 모델. 필요한 모델 필드를 추가하세요.
     """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='testpost_user'
+    )
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
@@ -11,6 +15,9 @@ class Post(models.Model):
 class Comment(models.Model):
     """댓글 모델. 필요한 모델 필드를 추가하세요.
     """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='testcomment_user'
+    )
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
