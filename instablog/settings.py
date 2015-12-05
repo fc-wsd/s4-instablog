2de89203ea3fa2743935d68540d8aa61ace04692
Kay Cha <kay@hannal.net>
2015-12-05 16:20:40 +0900

    add LOGGING 설정

--- instablog/settings.py
+++ instablog/settings.py
@@ -85,3 +85,29 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/new_world/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'  # noqa
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+    }
+}
