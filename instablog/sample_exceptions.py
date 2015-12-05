e7126c2a8a12fa1b601e5e10923b35ae4f92950c
Kay Cha <kay@hannal.net>
2015-12-05 14:15:54 +0900

    add HelloWorldError와 대응 middleware 메서드

---
+++ instablog/sample_exceptions.py
@@ -0,0 +1,4 @@
+
+
+class HelloWorldError(Exception):
+    pass
--- instablog/sample_middlewares.py
+++ instablog/sample_middlewares.py
@@ -1,6 +1,17 @@
 # sample_middlewares.py
 
+from django.shortcuts import render
+
+from instablog.sample_exceptions import HelloWorldError
+
 
 class SampleMiddleware(object):
     def process_request(self, request):
         request.just_say = 'Lorem Ipsum'
+
+    def process_exception(self, request, exc):
+        if isinstance(exc, HelloWorldError):
+            return render(request, 'error.html', {
+                'error': exc,
+                'status': 500,
+            })
